3d350469b81d958a7eca08e8b535e7e197159774
Sarah <alahmadisr@gmail.com>
2020-02-19 21:53:21 +0300

    fix get route error

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const auth=require('../../middleware/auth')
-const Profile=require('../../models/Profile')
-const User=require('../../models/User')
+const auth = require('../../middleware/auth')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
 // @access  Private 
 
 router.get('/me', auth, async (req, res) => {
-    try{
-        const profile=await (await Profile.findOne({user:req.user.id})).populate('User',['name','avatar'])
-        if(!profile){
-            return res.status(400).json({message: 'There is no profile for this user'})
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar'])
+        if (!profile) {
+            return res.status(400).json({ message: 'There is no profile for this user' })
         }
         res.json(profile) //if a profile was found, return it.
-    }catch(error){
+    } catch (error) {
         console.error(error.message)
         res.status(500).send('Server Error')
 
@@ -25,4 +25,4 @@ router.get('/me', auth, async (req, res) => {
 
 
 
-module.exports=router
+module.exports = router
